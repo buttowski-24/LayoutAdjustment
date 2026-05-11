754eb598d713e279663394791bb647641202598b
SathishSekar14 <sambuttowski21@outlook.com>
2026-05-11 15:38:26 +0530

    Adjust text frame geometric bounds based on page margins

--- layoutAdjustment.jsx
+++ layoutAdjustment.jsx
@@ -29,5 +29,18 @@ for (var k = 0; k < myMaster.length; k++) {
             if (myPage.marginPreferences.right < 36)
                 myPage.marginPreferences.right = 36;
         }
+
+        var mp = myPage.marginPreferences;
+        var pb = myPage.bounds; // [top, left, bottom, right]
+
+        var frameTop = pb[0] + mp.top;
+        var frameLeft = pb[1] + mp.left;
+        var frameBottom = pb[2] - mp.bottom;
+        var frameRight = pb[3] - mp.right;
+
+        var frames = myPage.textFrames;
+        for (var f = 0; f < frames.length; f++) {
+            frames[f].geometricBounds = [frameTop, frameLeft, frameBottom, frameRight];
+        }
     }
 }
